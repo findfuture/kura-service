ea1ac246e663a44b42f77201cf3876eb9993c51b
叶上夜 <317953536@qq.com>
2017-10-31 20:49:18 +0800

    V2.5.4

--- KURA/lib/Io.class.php
+++ KURA/lib/Io.class.php
@@ -30,6 +30,10 @@
                 return ($default !== '') ? $default : FALSE;
             }
             $val = $_POST[$key];
+            if (is_int($val))
+            {
+                return $val;
+            }
             if (is_array($val))
             {
                 $tmp = [];
@@ -66,6 +70,10 @@
                 $arr = explode('.', $val);
                 $val = $arr[0];
             }
+            if (is_int($val))
+            {
+                return $val;
+            }
             return self::_replace($val, $replace);
         }
         
@@ -75,8 +83,10 @@
             $url = $_SERVER['REQUEST_URI'];
             //拆分URL为数组
             $arr = explode('/', ltrim($url, '/'));
-            array_shift($arr);
-            array_shift($arr);
+            for ($i = 0; $i < (LEVEL + 2); $i++)
+            {
+                array_shift($arr);
+            }
             $k = array_search($key, $arr);
             if ($k === FALSE)
             {
@@ -143,33 +153,37 @@
                 $val = preg_replace('/(&#[xX]0{0,8}'.dechex(ord($search[$i])).';?)/i', $search[$i], $val);
                 $val = preg_replace('/(&#0{0,8}'.ord($search[$i]).';?)/', $search[$i], $val);
             }
-            $preg = [
-                'javascript', 
-                'vbscript', 
-                'expression', 
-                'applet', 
-                'meta', 
-                'xml', 
-                'blink', 
-                'link', 
-                'script', 
-                'embed', 
-                'object', 
-                'iframe', 
-                'frame', 
-                'frameset', 
-                'ilayer', 
-                'layer', 
-                'bgsound',
-                'base'
+            //屏蔽危险DOM
+            $dom = [
+                'javascript', 'vbscript', 'expression', 'applet', 'meta', 
+                'xml', 'blink', 'link', 'script', 'embed', 
+                'object', 'iframe', 'frame', 'frameset', 'ilayer', 
+                'layer', 'bgsound', 'base'
             ];
-            //替换黑名单规则
-            foreach ($preg as $p)
+            foreach ($dom as $d)
             {
-                $val = preg_replace('/[<|&lt;]+'.$p.'[>|&gt;]+(.*)[<|&lt;]+\/'.$p.'[>|&gt;]+/', '', $val);
+                $val = preg_replace('/[<|&lt;]+'.$d.'[>|&gt;]+(.*)[<|&lt;]+\/'.$d.'[>|&gt;]+/', '', $val);
             }
             //屏蔽事件
-            $val = preg_replace('/\s?on([a-z]+)=?/is', '', $val);
+            $event = [
+                'onabort', 'onactivate', 'onafterprint', 'onafterupdate', 'onbeforeactivate', 
+                'onbeforecopy', 'onbeforecut', 'onbeforedeactivate', 'onbeforeeditfocus', 'onbeforepaste', 
+                'onbeforeprint', 'onbeforeunload', 'onbeforeupdate', 'onblur', 'onbounce', 
+                'oncellchange', 'onchange', 'onclick', 'oncontextmenu', 'oncontrolselect', 
+                'oncopy', 'oncut', 'ondataavailable', 'ondatasetchanged', 'ondatasetcomplete', 
+                'ondblclick', 'ondeactivate', 'ondrag', 'ondragend', 'ondragenter', 
+                'ondragleave', 'ondragover', 'ondragstart', 'ondrop', 'onerror', 
+                'onerrorupdate', 'onfilterchange', 'onfinish', 'onfocus', 'onfocusin', 
+                'onfocusout', 'onhelp', 'onkeydown', 'onkeypress', 'onkeyup', 
+                'onlayoutcomplete', 'onload', 'onlosecapture', 'onmousedown', 'onmouseenter', 
+                'onmouseleave', 'onmousemove', 'onmouseout', 'onmouseover', 'onmouseup', 
+                'onmousewheel', 'onmove', 'onmoveend', 'onmovestart', 'onpaste', 
+                'onpropertychange', 'onreadystatechange', 'onreset', 'onresize', 'onresizeend', 
+                'onresizestart', 'onrowenter', 'onrowexit', 'onrowsdelete', 'onrowsinserted', 
+                'onscroll', 'onselect', 'onselectionchange', 'onselectstart', 'onstart', 
+                'onstop', 'onsubmit', 'onunload'
+            ];
+            $val = preg_replace('/'.implode('|', $event).'/is', '', $val);
             //字符串黑名单
             $black = [
                 'document.cookie' => '',
